88f54bef6d86d8cd642c0a622d5332fed14a97ad
Alberto Nicoletti <alberto.nicoletti@musixmatch.com>
2023-12-03 11:43:17 +0100

    refactor: day 01 - part 1

--- day_01/day_01.ml
+++ day_01/day_01.ml
@@ -1,32 +1,18 @@
 open Base
 open Stdio
 
-module Result_syntax = struct
-  let ( let* ) x f = Result.bind ~f x
-  let return = Result.return
-end
+let find_first_digit line =
+  String.find line ~f:Char.is_digit
+  |> Option.value_exn ~message:"There should always be a digit in the line"
+  |> Char.to_string
 
-module Parser = struct
-  open Angstrom
-
-  let num = take_while1 Char.is_digit
-  let chars = take_while Char.is_alpha
-
-  let numbers_of_line =
-    (fun numbers ->
-      List.hd_exn numbers ^ List.last_exn numbers |> Int.of_string)
-    <$> many (chars *> num <* chars)
-
-  let lines = sep_by end_of_line numbers_of_line
-  let run contents = parse_string ~consume:Consume.All lines contents
-end
-
-let part_1_res =
-  let open Result_syntax in
-  let* input = Parser.run (In_channel.read_all "test.txt") in
-  List.fold input ~init:0 ~f:( + ) |> return
+let get_number_of_line line =
+  let first_digit = find_first_digit line in
+  let last_digit = find_first_digit (String.rev line) in
+  first_digit ^ last_digit |> Int.of_string
 
 let () =
-  match part_1_res with
-  | Ok result -> print_endline (Int.to_string result)
-  | Error err -> print_endline err
+  let lines = In_channel.read_lines "input.txt" in
+  let numbers = List.map lines ~f:get_number_of_line in
+  let part_1 = List.fold numbers ~init:0 ~f:( + ) in
+  print_endline ("Part 1: " ^ Int.to_string part_1)
