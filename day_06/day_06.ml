b6aecf2ac1c156c23b8fdd288fa9814b07f09755
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2023-12-06 09:58:10 +0100

    feat(day6): part 2

--- day_06/day_06.ml
+++ day_06/day_06.ml
@@ -21,18 +21,29 @@ module Parser = struct
   let run = parse_string races ~consume:All
 end
 
+let get_final_race races =
+  List.fold races ~init:{ distance = 0; time = 0 } ~f:(fun acc curr ->
+      {
+        distance =
+          Int.to_string acc.distance ^ Int.to_string curr.distance
+          |> Int.of_string;
+        time = Int.to_string acc.time ^ Int.to_string curr.time |> Int.of_string;
+      })
+
+let process_race race =
+  List.range 0 race.time ~stop:`inclusive
+  |> List.map ~f:(fun hold_button_ms ->
+         let ms_of_movement = race.time - hold_button_ms in
+         let movement = ms_of_movement * hold_button_ms in
+         movement)
+  |> List.filter ~f:(fun movement -> movement > race.distance)
+  |> List.length
+
 let () =
   let input = In_channel.read_all "input.txt" in
   let races = Parser.run input |> Result.ok_or_failwith in
-  let part_1 =
-    List.map races ~f:(fun { time; distance } ->
-        List.range 0 time ~stop:`inclusive
-        |> List.map ~f:(fun hold_button_ms ->
-               let ms_of_movement = time - hold_button_ms in
-               let movement = ms_of_movement * hold_button_ms in
-               movement)
-        |> List.filter ~f:(fun movement -> movement > distance)
-        |> List.length)
-    |> List.fold ~init:1 ~f:( * )
-  in
-  printf "Part 1: %i\n" part_1
+  let part_1 = List.map races ~f:process_race |> List.fold ~init:1 ~f:( * ) in
+  print_endline @@ "Part 1: " ^ Int.to_string part_1;
+  let final_race = get_final_race races in
+  let part_2 = process_race final_race in
+  print_endline @@ "Part 2: " ^ Int.to_string part_2
