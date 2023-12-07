7e446476e200566a729d1eb2d8a8bc3699bbc100
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2023-12-07 22:51:24 +0100

    feat(day07): part 2

--- day_07/day_07.ml
+++ day_07/day_07.ml
@@ -6,23 +6,18 @@ module Card = struct
 
   let from_char str = { kind = str }
 
-  let value = function
-    | { kind = 'A' } -> 13
-    | { kind = 'K' } -> 12
-    | { kind = 'Q' } -> 11
-    | { kind = 'J' } -> 10
-    | { kind = 'T' } -> 9
-    | { kind = '9' } -> 8
-    | { kind = '8' } -> 7
-    | { kind = '7' } -> 6
-    | { kind = '6' } -> 5
-    | { kind = '5' } -> 4
-    | { kind = '4' } -> 3
-    | { kind = '3' } -> 2
-    | { kind = '2' } -> 1
+  let value ~is_joker card =
+    match card with
+    | { kind = 'A' } -> 14
+    | { kind = 'K' } -> 13
+    | { kind = 'Q' } -> 12
+    | { kind = 'J' } -> if is_joker then 1 else 11
+    | { kind = 'T' } -> 10
+    | { kind = '2' .. '9' } -> card.kind |> Char.to_string |> Int.of_string
     | _ -> 0
 
-  let compare a b = Int.compare (value a) (value b)
+  let compare a b ~is_joker =
+    Int.compare (value a ~is_joker) (value b ~is_joker)
 end
 
 module Hand = struct
@@ -42,23 +37,46 @@ module Hand = struct
     let cards = str |> String.to_list |> List.map ~f:Card.from_char in
     { cards }
 
-  let get_type t =
-    let sorted = List.sort ~compare:Card.compare t.cards in
-    let grouped = List.group sorted ~break:(fun a b -> Card.compare a b <> 0) in
+  let show t =
+    List.map t.cards ~f:(fun card -> card.kind) |> String.of_char_list
+
+  let get_type t ~is_joker =
+    let cards =
+      if is_joker then
+        List.filter t.cards ~f:(fun card -> not (Char.equal card.kind 'J'))
+      else t.cards
+    in
+    let sorted = List.sort ~compare:(Card.compare ~is_joker) cards in
+    let grouped =
+      List.group sorted ~break:(fun a b -> Card.compare a b ~is_joker <> 0)
+    in
     let counts = List.map grouped ~f:(fun group -> List.length group) in
-    let max_count = List.max_elt counts ~compare:Int.compare in
-    match max_count with
-    | Some 5 -> Five_of_a_kind
-    | Some 4 -> Four_of_a_kind
-    | Some 3 -> if List.length counts = 2 then Full_house else Three_of_a_kind
-    | Some 2 -> if List.length counts = 3 then Two_pair else One_pair
+    let max_count =
+      List.max_elt counts ~compare:Int.compare |> Option.value ~default:0
+    in
+    match
+      max_count
+      +
+      if is_joker then
+        let num_of_jokers =
+          List.count t.cards ~f:(fun card -> Char.equal card.kind 'J')
+        in
+        num_of_jokers
+      else 0
+    with
+    | 5 -> Five_of_a_kind
+    | 4 -> Four_of_a_kind
+    | 3 -> if List.length counts = 2 then Full_house else Three_of_a_kind
+    | 2 -> if List.length counts = 3 then Two_pair else One_pair
     | _ -> High_card
 
-  let compare a b =
-    match Stdlib.compare (get_type a) (get_type b) with
+  let compare a b ~is_joker =
+    match Stdlib.compare (get_type a ~is_joker) (get_type b ~is_joker) with
     | 0 ->
         let zipped = List.zip_exn a.cards b.cards in
-        let compared = List.map zipped ~f:(fun (a, b) -> Card.compare a b) in
+        let compared =
+          List.map zipped ~f:(fun (a, b) -> Card.compare a b ~is_joker)
+        in
         List.find compared ~f:(fun v -> v <> 0) |> Option.value ~default:0
     | v -> v
 end
@@ -80,9 +98,10 @@ module Bets = struct
     String.split_lines str |> List.map ~f:Bet.from_string |> fun bets ->
     { bets }
 
-  let total_winnings (t : t) =
+  let total_winnings t ~is_joker =
     let sorted =
-      List.sort t.bets ~compare:(fun a b -> Hand.compare a.hand b.hand)
+      List.sort t.bets ~compare:(fun a b ->
+          Hand.compare a.hand b.hand ~is_joker)
     in
     let ranks = List.mapi sorted ~f:(fun i bet -> bet.bet * (i + 1)) in
     List.fold ranks ~init:0 ~f:( + )
@@ -91,7 +110,7 @@ end
 let () =
   let input = In_channel.read_all "input.txt" in
   let bets = Bets.from_string input in
-  let part_1 = Bets.total_winnings bets in
-  print_endline @@ "Part 1: " ^ Int.to_string part_1;
-  let part_2 = 0 in
-  print_endline @@ "Part 2: " ^ Int.to_string part_2
+  let part_1 = Bets.total_winnings ~is_joker:false bets in
+  printf "Part 1: %s\n" @@ Int.to_string part_1;
+  let part_2 = Bets.total_winnings ~is_joker:true bets in
+  printf "Part 2: %s\n" @@ Int.to_string part_2
