d7da8fd8f104d7fcbbd8fe6ef4888dbfd3575664
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2023-12-10 14:19:31 +0100

    feat(day_09): part 2

--- day_09/day_09.ml
+++ day_09/day_09.ml
@@ -8,20 +8,33 @@ module Parser = struct
            String.split line ~on:' ' |> List.map ~f:Int.of_string)
 end
 
-let rec extrapolate_right history =
-  let last_value = List.last_exn history in
+let rec extrapolate history ~direction =
+  let value =
+    match direction with
+    | `Left -> List.hd_exn history
+    | `Right -> List.last_exn history
+  in
   let zipped =
     List.zip_exn (List.tl_exn history) (List.drop_last_exn history)
   in
   let differences = List.map zipped ~f:(fun (a, b) -> a - b) in
-  if List.for_all differences ~f:(fun x -> x = 0) then last_value
-  else last_value + extrapolate_right differences
+  if List.for_all differences ~f:(fun x -> x = 0) then value
+  else
+    let inner_extrapolation = extrapolate differences ~direction in
+    match direction with
+    | `Left -> value - inner_extrapolation
+    | `Right -> value + inner_extrapolation
 
 let () =
   let input = In_channel.read_all "input.txt" in
   let histories = Parser.run input in
-  let extrapolated_values = List.map histories ~f:extrapolate_right in
-  let part_1 = List.fold extrapolated_values ~init:0 ~f:( + ) in
+  let part_1 =
+    List.map histories ~f:(extrapolate ~direction:`Right)
+    |> List.fold ~init:0 ~f:( + )
+  in
   printf "Part 1: %i\n" part_1;
-  let part_2 = 0 in
+  let part_2 =
+    List.map histories ~f:(extrapolate ~direction:`Left)
+    |> List.fold ~init:0 ~f:( + )
+  in
   printf "Part 2: %i\n" part_2
